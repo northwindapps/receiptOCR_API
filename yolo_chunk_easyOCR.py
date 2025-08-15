2e7f912bc6d50ccbefa044658cfb08b0948362ed
northwindapps <northwindapps@gmail.com>
2025-08-15 13:10:54 +0900

    updated totalLabel-- added countour area param

--- yolo_chunk_easyOCR.py
+++ yolo_chunk_easyOCR.py
@@ -9,8 +9,33 @@ chunk_model = YOLO('text_chunk_epoch40_best.pt')
 reader = easyocr.Reader(['en'], gpu=False)  # Set gpu=True if you have a compatible GPU
 
 # Image path
-image_path = r'C:\Users\ABC\Documents\receiptYOLOProject\test7.jpg'
+image_path = r'C:\Users\ABC\Documents\receiptYOLOProject\test16_1_cropped.jpg'
 image = cv2.imread(image_path)
+
+ # Crop chunk from original image
+# chunk_crop = image[y_min:y_max, x_min:x_max]
+
+# Parameters to tweak sharpness
+strength = 1.5  # How much to boost edges (1.0 = no change)
+blur_size = (0, 0)  # Let OpenCV figure size from sigma
+sigma = 3  # How much to blur before subtracting
+
+# Unsharp masking
+blurred = cv2.GaussianBlur(image, blur_size, sigma)
+sharpened = cv2.addWeighted(image, 1 + strength, blurred, -strength, 0)
+
+
+# EasyOCR expects RGB images
+chunk_rgb = cv2.cvtColor(sharpened, cv2.COLOR_BGR2RGB)
+
+results = reader.readtext(chunk_rgb, detail=1)
+
+for bbox, text, confidence in results:
+    print(f"Chunk Text: {text}")
+    print(f"Confidence: {confidence:.2f}")
+    # print(f"Chunk BBox: [{x_min}, {y_min}, {x_max}, {y_max}]")
+    print("=" * 40)
+
 
 # Run chunk detection on full image
 chunk_results = chunk_model(image_path, conf=0.2, save=True, show=True)
--- yolo_totalLabel.py
+++ yolo_totalLabel.py
@@ -10,7 +10,7 @@ totalLabel_model = YOLO('totalLabel_best.pt')
 chunk_model = YOLO('text_chunk_epoch40_best.pt')
 
 # Image path
-image_path = r'C:\Users\ABC\Documents\receiptYOLOProject\test13.jpg'
+image_path = r'C:\Users\ABC\Documents\receiptYOLOProject\test21.jpg'
 image = cv2.imread(image_path)
 
 # Parameters to tweak sharpness
@@ -36,10 +36,12 @@ pytesseract.pytesseract.tesseract_cmd = r"C:\Program Files\Tesseract-OCR\tessera
 reader = easyocr.Reader(['en'], gpu=False)  # Change gpu=True if you have a GPU and want to use it
 
  # Define contrast & brightness variations to try
-alpha_values = [-3.0,-1.0,1.5,3.0,5.0]  # contrast
-beta_values = [-80,-50,0,50,80,160]  # brightness
-
-
+# alpha_values = [-3.0,-1.0,1.5,3.0,5.0]  # contrast
+# beta_values = [-80,-50,0,50,80,160]  # brightness
+alpha_values = [1.5]  # contrast
+# beta_values = [-80,-50,0,50,80,160]
+beta_values = [80,50,0,-50,-80]
+cont_area_values = [10,15,35,45]
 
 # Run layout detection
 totalLabel_results = totalLabel_model(source=sharpened, conf=0.50, save=True, show=True)
@@ -52,14 +54,14 @@ for idx, totalLabel_result in enumerate(totalLabel_results):
         x_min, y_min, x_max, y_max = map(int, totalLabel_box.xyxy[0].tolist())
 
         stop_early = False  # flag to exit all loop
-        target_conf = 0.8
+        target_conf = 0.9
         best_text = ""
         best_conf = 0
         prev = ""
         count = 0
 
         # Loop through margin adjustments
-        for margin_adjust in [-5,-3,-2,-1, 0, 1, 2,3,5]:
+        for margin_adjust in [-5,-3,-1, 0, 1,3,5]:
             y_min_adj = max(0, y_min - margin_adjust)
             y_max_adj = y_max
             if stop_early:
@@ -68,121 +70,131 @@ for idx, totalLabel_result in enumerate(totalLabel_results):
             # Loop through all alpha/beta combinations
             for alpha in alpha_values:
                 for beta in beta_values:
-                    # Apply brightness & contrast adjustment to original sharpened image
-                    bc_image = cv2.convertScaleAbs(src=sharpened, alpha=alpha, beta=beta)
+                    for cont_value in cont_area_values:
+                        # Apply brightness & contrast adjustment to original sharpened image
+                        bc_image = cv2.convertScaleAbs(src=sharpened, alpha=alpha, beta=beta)
 
-                    layout_crop = bc_image[int(y_min_adj):int(y_max_adj), int(x_max+0):img_width]
-                    scale_factor = 3
-                    resized_chunk = cv2.resize(layout_crop, None, fx=scale_factor, fy=scale_factor, interpolation=cv2.INTER_CUBIC)
+                        layout_crop = bc_image[int(y_min_adj):int(y_max_adj), int(x_max+0):img_width]
+                        scale_factor = 3
+                        resized_chunk = cv2.resize(layout_crop, None, fx=scale_factor, fy=scale_factor, interpolation=cv2.INTER_CUBIC)
 
-                    margin = 20
-                    if len(resized_chunk.shape) == 2:
-                        chunk_crop_with_bg = cv2.copyMakeBorder(
-                            resized_chunk, margin, margin, margin, margin,
-                            cv2.BORDER_CONSTANT, value=255
-                        )
-                    else:
-                        chunk_crop_with_bg = cv2.copyMakeBorder(
-                            resized_chunk, margin, margin, margin, margin,
-                            cv2.BORDER_CONSTANT, value=[255, 255, 255]
-                        )
+                        margin = 20
+                        if len(resized_chunk.shape) == 2:
+                            chunk_crop_with_bg = cv2.copyMakeBorder(
+                                resized_chunk, margin, margin, margin, margin,
+                                cv2.BORDER_CONSTANT, value=255
+                            )
+                        else:
+                            chunk_crop_with_bg = cv2.copyMakeBorder(
+                                resized_chunk, margin, margin, margin, margin,
+                                cv2.BORDER_CONSTANT, value=[255, 255, 255]
+                            )
 
-                    gray = cv2.cvtColor(chunk_crop_with_bg, cv2.COLOR_BGR2GRAY)
-                    _, thresh = cv2.threshold(gray, 150, 255, cv2.THRESH_BINARY)
+                        gray = cv2.cvtColor(chunk_crop_with_bg, cv2.COLOR_BGR2GRAY)
+                        _, thresh = cv2.threshold(gray, 150, 255, cv2.THRESH_BINARY)
 
-                    # Remove small blobs
-                    contours, _ = cv2.findContours(thresh, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-                    for cnt in contours:
-                        if cv2.contourArea(cnt) < 15:
-                            cv2.drawContours(thresh, [cnt], -1, 0, -1)
+                        # Remove small blobs
+                        contours, _ = cv2.findContours(thresh, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+                        for cnt in contours:
+                            if cv2.contourArea(cnt) < cont_value:
+                                cv2.drawContours(thresh, [cnt], -1, 0, -1)
 
-                    # Save the cropped image
-                    crop_filename = os.path.join(save_dir, f"totalLabel_crop_{idx}_{jdx}.png")
-                    cv2.imwrite(crop_filename, thresh)
-                    print(f"Saved crop: {crop_filename}")
+                        # Save the cropped image
+                        crop_filename = os.path.join(save_dir, f"totalLabel_crop_{idx}_{jdx}.png")
+                        cv2.imwrite(crop_filename, thresh)
+                        print(f"Saved crop: {crop_filename}")
 
-                    # Run Tesseract
-                    data = pytesseract.image_to_data(thresh, lang='eng', output_type=pytesseract.Output.DICT)
-                    avg_conf = sum(int(c) for c in data['conf'] if int(c) >= 0) / max(1, len([c for c in data['conf'] if int(c) >= 0]))
-                    avg_conf /= 100
+                        # Run Tesseract
+                        data = pytesseract.image_to_data(thresh, lang='eng', output_type=pytesseract.Output.DICT)
+                        avg_conf = sum(int(c) for c in data['conf'] if int(c) >= 0) / max(1, len([c for c in data['conf'] if int(c) >= 0]))
+                        avg_conf /= 100
 
-                    text = pytesseract.image_to_string(thresh, lang='eng', config="--psm 7 -c tessedit_char_whitelist=0123456789:.$").strip()
-                    # text = pytesseract.image_to_string(thresh, lang='eng').strip()
+                        text = pytesseract.image_to_string(thresh, lang='eng', config="--psm 7 -c tessedit_char_whitelist=0123456789:.$").strip()
+                        # text = pytesseract.image_to_string(thresh, lang='eng').strip()
 
 
-                    if avg_conf > best_conf:
-                        best_conf = avg_conf
-                        best_text = text
-                    if avg_conf < 0.6:
-                        break
-                
-                if stop_early:
-                            break 
-                # After trying all alpha/beta/margin combinations
-                if best_conf >= target_conf:
-                    print(f"Final OCR Text: {best_text}, Confidence: {best_conf:.2f}")
-                    print(f"Box: [{x_min}, {y_min}, {x_max}, {y_max}]")
-                else:
-                    print(f"No high-confidence result found, best was: {best_text} ({best_conf:.2f})")
-
-                if best_conf >= 0.85 and any(char.isdigit() for char in best_text) and "." in best_text:
-                    stop_early = True
-                    print(f"alpha:{alpha}")
-                    print(f"beta:{beta}")
-                    print("Contains numbers")
-                    break
-                elif best_conf >= 0.75 and any(char.isdigit() for char in best_text)  and "$" in best_text and "." in best_text:
-                    stop_early = True
-                    print(f"alpha:{alpha}")
-                    print(f"beta:{beta}")
-                    print("Contains numbers")
-                    break
-                elif best_conf >= 0.70 and any(char.isdigit() for char in best_text) and count > 2 and "." in best_text:
-                    stop_early = True
-                    print(f"alpha:{alpha}")
-                    print(f"beta:{beta}")
-                    prev = ''
-                    count = 0
-                    break
-                else:
-                    if prev == best_text:
+                        if avg_conf > best_conf:
+                            best_conf = avg_conf
+                            best_text = text
+                        if avg_conf < 0.6:
+                            break
+                    
+                    if stop_early:
+                                break 
+                    if best_conf >= 0.75 and prev == best_text:
                         count += 1
                     else:
                         prev = best_text
-                    # Convert BGR to RGB for EasyOCR
-                    chunk_rgb = cv2.cvtColor(thresh, cv2.COLOR_BGR2RGB)
-
-                    # OCR with EasyOCR
-                    results = reader.readtext(chunk_rgb, detail=1)
-
-                    for bbox, text, confidence in results:
-                        if stop_early:
-                            break 
-                        
-                        # print(f"Chunk BBox: [{abs_x_min}, {abs_y_min}, {abs_x_max}, {abs_y_max}]")
-
-                        clean_text = text.replace("-", ".")
-                        if any(char.isdigit() for char in clean_text) and not clean_text.endswith("."):
-                            if confidence >= 0.85 and "." in clean_text:
-                                stop_early = True
-                                print(f"Chunk Text: {clean_text}")
-                                print(f"Confidence: {confidence:.2f}")
-                                print(f"Chunk BBox: [{x_min}, {y_min}, {x_max}, {y_max}]")
-                                print(f"alpha:{alpha}")
-                                print(f"beta:{beta}")
-                                print("Contains numbers")
-                                break
-                            if confidence >= 0.7 and "$" in clean_text and "." in clean_text:
-                                stop_early = True
-                                print(f"Chunk Text: {clean_text}")
-                                print(f"Confidence: {confidence:.2f}")
-                                print(f"Chunk BBox: [{x_min}, {y_min}, {x_max}, {y_max}]")
-                                print(f"alpha:{alpha}")
-                                print(f"beta:{beta}")
-                                print("Contains $")
-                                break
-
+                        count = 0
+                    # After trying all alpha/beta/margin combinations
+                    if best_conf >= target_conf:
+                        print(f"Final OCR Text: {best_text}, Confidence: {best_conf:.2f}")
+                        print(f"Box: [{x_min}, {y_min}, {x_max}, {y_max}]")
                     else:
-                        print("No numbers found")
+                        print(f"No high-confidence result found, best was: {best_text} ({best_conf:.2f})")
+                        if best_conf == 0.00:
+                            count -= 1
 
-                    
+                    if best_conf > 0.90 and any(char.isdigit() for char in best_text) and "." in best_text:
+                        stop_early = True
+                        print(f"alpha:{alpha}")
+                        print(f"beta:{beta}")
+                        print("Contains numbers")
+                        break
+                    # elif best_conf >= 0.75 and any(char.isdigit() for char in best_text)  and "$" in best_text and "." in best_text:
+                    #     stop_early = True
+                    #     print(f"alpha:{alpha}")
+                    #     print(f"beta:{beta}")
+                    #     print("Contains numbers")
+                    #     break
+                    elif best_conf >= 0.75 and any(char.isdigit() for char in best_text) and count > 3 and "." in best_text:
+                        stop_early = True
+                        print(f"alpha:{alpha}")
+                        print(f"beta:{beta}")
+                        prev = ''
+                        count = 0
+                        break
+                    elif count < -5:
+                        stop_early = True
+                        prev = ''
+                        count = 0
+                        break
+                    else:
+                        print("easyOCR")
+                        # Convert BGR to RGB for EasyOCR
+                        chunk_rgb = cv2.cvtColor(thresh, cv2.COLOR_BGR2RGB)
+
+                        # OCR with EasyOCR
+                        results = reader.readtext(chunk_rgb, detail=1)
+
+                        for bbox, text, confidence in results:
+                            if stop_early:
+                                break 
+                            
+                            # print(f"Chunk BBox: [{abs_x_min}, {abs_y_min}, {abs_x_max}, {abs_y_max}]")
+
+                            clean_text = text.replace("-", ".")
+                            if any(char.isdigit() for char in clean_text) and not clean_text.endswith(".") and not clean_text.startswith("."):
+                                if confidence >= 0.9 and "." in clean_text:
+                                    stop_early = True
+                                    print(f"Chunk Text: {clean_text}")
+                                    print(f"Confidence: {confidence:.2f}")
+                                    print(f"Chunk BBox: [{x_min}, {y_min}, {x_max}, {y_max}]")
+                                    print(f"alpha:{alpha}")
+                                    print(f"beta:{beta}")
+                                    print("Contains numbers")
+                                    break
+                                if confidence >= 0.7 and "$" in clean_text and "." in clean_text:
+                                    stop_early = True
+                                    print(f"Chunk Text: {clean_text}")
+                                    print(f"Confidence: {confidence:.2f}")
+                                    print(f"Chunk BBox: [{x_min}, {y_min}, {x_max}, {y_max}]")
+                                    print(f"alpha:{alpha}")
+                                    print(f"beta:{beta}")
+                                    print("Contains $")
+                                    break
+
+                        else:
+                            print("No numbers found")
+
+                        
