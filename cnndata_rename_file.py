b7e96fe9db9c442bb74506f4734d3ef8c8866e6c
northwindapps <northwindapps@gmail.com>
2025-08-23 00:16:34 +0900

    updated rotate py

--- cnndata_rename_file.py
+++ cnndata_rename_file.py
@@ -1,8 +1,9 @@
-import os
+import os,json,re
 
-folder = r"C:\Users\ABC\Documents\receiptYOLOProject\cnndata\images"
-text_file = r"C:\Users\ABC\Documents\receiptYOLOProject\cnndata\labels.txt"
+folder = r"C:\Users\ABC\Documents\receiptYOLOProject\2025_0822_bad_cnndata\cnndata\images"
+text_file = r"C:\Users\ABC\Documents\receiptYOLOProject\cnndata\labels.json"
 name_list = []
+data = []
 for idx,fname in enumerate(os.listdir(folder)):
     name_list.append(fname)
     # if "_pyttext_" not in fname:
@@ -22,12 +23,18 @@ for idx,fname in enumerate(os.listdir(folder)):
     # print(f"Renaming: {fname} -> {new_name}")
     # os.rename(old_path, new_path)
 
+for fname in name_list:
+    if "labels"  in fname:
+        continue  # skip files without marker
+    chunks = fname.split("_")
+    annotation = chunks[3]
+    cleaned = re.sub(r"[A-Za-z\s]", "", annotation)
+    print(cleaned)
+    data.append({
+        "filename": fname,
+        "annotation": cleaned
+    })
+
 with open(text_file, "w", encoding="utf-8") as f:
-    for fname in name_list:
-        if "labels"  in fname:
-            continue  # skip files without marker
-        chunks = fname.split("_")
-        annotation = chunks[1]
-        line = f"{fname},{annotation}\n"
-        f.write(line)
+    f.write(json.dumps(data, ensure_ascii=False, indent=4))
 
--- rotate.py
+++ rotate.py
@@ -1,19 +1,38 @@
-import cv2
+import cv2,os,glob
 import numpy as np
 
 # Read image
-image_path = r'C:\Users\ABC\Documents\receiptYOLOProject\chunk_crop_0_10.png'
-image = cv2.imread(image_path)
+image_dir = r"C:\Users\ABC\Documents\receiptYOLOProject\2025_0822_bad_cnndata\cnndata\images\tmp"
 
-# Get image dimensions
-(h, w) = image.shape[:2]
-center = (w // 2, h // 2)
+def rotate(deg=0.0,image_path=""):
+    image = cv2.imread(image_path)
+    # Get image dimensions
+    (h, w) = image.shape[:2]
+    center = (w // 2, h // 2)
 
-# Create rotation matrix (positive angle = counter-clockwise)
-M = cv2.getRotationMatrix2D(center, -0.5, 1.0)  # angle=2 degrees, scale=1.0
+    # Create rotation matrix (positive angle = counter-clockwise)
+    M = cv2.getRotationMatrix2D(center, deg, 1.0)  # angle=2 degrees, scale=1.0
 
-# Apply rotation
-rotated = cv2.warpAffine(image, M, (w, h), flags=cv2.INTER_LINEAR, borderValue=(255, 255, 255))
+    # Apply rotation
+    rotated = cv2.warpAffine(image, M, (w, h), flags=cv2.INTER_LINEAR, borderValue=(255, 255, 255))
 
-# Save or show
-cv2.imwrite("rotated.jpg", rotated)
+    # Save or show
+    return rotated
+   
+
+# Get all image files in the folder (jpg, png, etc.)
+# Match both upper/lowercase JPG/PNG
+image_files = glob.glob(os.path.join(image_dir, "*.jpg"))
+image_files += glob.glob(os.path.join(image_dir, "*.jpeg"))
+image_files += glob.glob(os.path.join(image_dir, "*.png"))
+rotate_degrees = [-5.0,-3.0, -1.0, 1.0, 3.0,5.0]
+
+for img_path in image_files:
+    filename = os.path.splitext(os.path.basename(img_path))[0]
+    for idx,r in enumerate(rotate_degrees):
+        rotated = rotate(r, img_path)
+        if rotated is not None:
+            save_path = os.path.join(image_dir, f"{filename}_rot{idx}_{r}.jpg")
+            cv2.imwrite(save_path, rotated)
+            print(f"✅ Saved {save_path}")
+
