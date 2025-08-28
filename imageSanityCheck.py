08dfb3e5057a4b86ffcbf374582d0fbe6a7c2707
northwindapps <northwindapps@gmail.com>
2025-08-28 11:34:16 +0900

    updated jupyternotebook with validation,added dataset healthy check scripts

--- imageSanityCheck.py
+++ imageSanityCheck.py
@@ -1,21 +1,28 @@
-import random, cv2,os
+import json
+import cv2
+import random
+import os
 
+# Paths
 folder = r"C:\Users\ABC\Documents\cnndata\images"
-image_paths=[]
-texts=[]
-for fname in os.listdir(folder):
-    full_path = os.path.join(folder, fname)
-    image_paths.append(full_path)
-    chunks = fname.split("_")
-    annotation = chunks[1]
-    annotation = annotation.replace("jpy","￥")
-    texts.append(annotation)
+text_file = r"C:\Users\ABC\Documents\cnndata\labels.json"
 
+# Load JSON
+with open(text_file, "r", encoding="utf-8") as f:
+    data = json.load(f)
+
+# Extract image paths and labels
+image_paths = [os.path.join(folder, item["filename"]) for item in data]
+texts = [item["annotation"] for item in data]
+
+# Show 10 random samples
 for p, lbl in random.sample(list(zip(image_paths, texts)), 10):
     print("GT:", lbl)
     im = cv2.imread(p)
-    if im is None: 
-        print("can't open", p); continue
+    if im is None:
+        print("Can't open", p)
+        continue
     cv2.imshow("sample", cv2.resize(im, (im.shape[1], im.shape[0])))
     cv2.waitKey(2500)  # or save to disk if headless
+
 cv2.destroyAllWindows()
