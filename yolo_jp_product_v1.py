e6887bb947f16d618b224a664e3131b4ffaf569c
northwindapps <northwindapps@gmail.com>
2025-09-14 22:15:14 +0900

    ready to make api

--- yolo_jp_product_v1.py
+++ yolo_jp_product_v1.py
@@ -18,6 +18,7 @@ os.makedirs(save_original_dir, exist_ok=True)
 # Need preprocess for dark image... need @ annotation. you can expect better score with this
 image_path = r"C:\Users\ABC\Documents\receiptYOLOProject\IMG_0946_.jpg"
 image_path = r"C:\Users\ABC\Documents\receiptYOLOProject\IMG_0944_.jpg"
+image_path = r"C:\Users\ABC\Documents\receiptYOLOProject\IMG_0942_.jpg"
 # image_path = r"C:\Users\ABC\Documents\receiptYOLOProject\IMG_0953_.jpg"
 # image_path = r"C:\Users\ABC\Documents\receiptYOLOProject\IMG_0950_.jpg"
 # image_path = r"C:\Users\ABC\Documents\receiptYOLOProject\IMG_0940_.jpg"
@@ -31,27 +32,23 @@ image_path = r"C:\Users\ABC\Documents\receiptYOLOProject\IMG_0944_.jpg"
 image = cv2.imread(image_path)
 
 # Load the models
-# parse_model_path = r"C:\Users\ABC\Documents\receiptYOLOProject\crnn_model_val0.14_stable.keras"
-# parse_model_path = r"C:\Users\ABC\Documents\receiptYOLOProject\crnn_jpy_vllos0.14crnn_model.keras"
-parse_model_path = r"C:\Users\ABC\Documents\receiptYOLOProject\crnn_model_vallos0.19_jpy.keras"
-parse_model_path = r"C:\Users\ABC\Documents\receiptYOLOProject\crnn_model_jpy_val0.3.keras"
-parse_model_path = r"C:\Users\ABC\Documents\receiptYOLOProject\crnn_model_1k_valloss0.3_jpy.keras"
-parse_model_path = r"C:\Users\ABC\Documents\receiptYOLOProject\crnn_model_@_jpy_0.2757.keras"
 parse_model_path = r"C:\Users\ABC\Documents\receiptYOLOProject\crnn_model_jpy_valloss0.2_10k.keras"
 parse_model = load_model(parse_model_path, compile=False)
 crop_model_path = r"C:\Users\ABC\Documents\receiptYOLOProject\date_telephone_jp_best.pt"
-# crop_model_path = r"C:\Users\ABC\Documents\receiptYOLOProject\text_chunk_epoch40_best.pt"
-crop_model = YOLO(crop_model_path)
-# Definately need drastic improvment!
-crop_results = crop_model(source=image, conf=0.1, save=True, show=True)
+crop_model_path2 = r"C:\Users\ABC\Documents\receiptYOLOProject\text_chunk_epoch40_best.pt"
+crop_model_tel_date = YOLO(crop_model_path)
+crop_model_all = YOLO(crop_model_path2)
+
+crop_tel_date_results = crop_model_tel_date(source=image, conf=0.1, save=True, show=True)
+crop_all_results = crop_model_all(source=image, conf=0.1, save=True, show=True)
+
 
 all_boxes = [
     (idx, jdx, box) 
-    for idx, crop_result in enumerate(crop_results)
+    for idx, crop_result in enumerate(crop_tel_date_results)
     for jdx, box in enumerate(crop_result.boxes)
 ]
-#Move to backup layer            
-# if not total_label_cords and not total_value_texts: which should I choose?
+
 for idx, jdx, total_box in all_boxes:
     x_min, y_min, x_max, y_max = map(int, total_box.xyxy[0].tolist())
     crop = image[int(y_min):int(y_max), int(x_min):int(x_max)]  
@@ -69,10 +66,74 @@ for idx, jdx, total_box in all_boxes:
 
     h, w = thresh.shape[:2]
 
-    # I want @170.00 170円 here
+    if w/h < 2.0: 
+        continue
+
+    target_h = 31
+
+    # scale based on height
+    if h > target_h or h < target_h:
+        scale = target_h / h
+        w = int(w * scale)
+        h = target_h
+        thresh = cv2.resize(thresh, (w, target_h))
+
+    # Remove small blobs
+    contours, _ = cv2.findContours(thresh, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+    for cnt in contours:
+        if cv2.contourArea(cnt) < 55:
+            cv2.drawContours(thresh, [cnt], -1, 0, -1)
+    vocab = "0123456789.,-$;/@￥年月日円() " # Added space to vocab
+    char_to_idx = {c:i+1 for i,c in enumerate(vocab)}  # 0 reserved for blank
+    idx_to_char = {i+1:c for i,c in enumerate(vocab)}
+
+    img = thresh.astype(np.float32) / 255.0
+    img = np.expand_dims(img, axis=-1)            # add channel dimension (H x W x 1)
+    img = np.expand_dims(img, axis=0)             # add batch dimension (1 x H x W x 1)
+    preds = parse_model.predict(img)
+    decoded, _ = tf.keras.backend.ctc_decode(preds, input_length=np.ones(preds.shape[0])*preds.shape[1], greedy=True)
+
+    decoded_indices = decoded[0].numpy()[0]
+    # decoded_text = [idx_to_char[i] for i in decoded_indices if i > 0]  # skip 0 and negatives
+    decoded_text = [idx_to_char.get(int(i), "?") for i in decoded_indices if i > 0]
+    decoded_str = "".join(decoded_text)
+    print("Decoded:", decoded_str)
+    
+
+    # Save the cropped image
+    timestamp = datetime.datetime.now().strftime("%Y%m%d_%H%M%S")
+    crop_filename = os.path.join(save_original_dir, f"0_{decoded_str}_{idx}_{jdx}_{timestamp}_original.png")
+    cv2.imwrite(crop_filename, thresh)
+    print(f"Saved crop: {crop_filename}")
+
+
+all_boxes = [
+    (idx, jdx, box) 
+    for idx, crop_result in enumerate(crop_all_results)
+    for jdx, box in enumerate(crop_result.boxes)
+]
+
+for idx, jdx, total_box in all_boxes:
+    x_min, y_min, x_max, y_max = map(int, total_box.xyxy[0].tolist())
+    crop = image[int(y_min):int(y_max), int(x_min):int(x_max)]  
+
+    g = cv2.cvtColor(crop, cv2.COLOR_BGR2GRAY)
+                            
+    # thresh = cv2.adaptiveThreshold(g, 255, cv2.ADAPTIVE_THRESH_GAUSSIAN_C, cv2.THRESH_BINARY, 11, 9)
+    thresh = cv2.adaptiveThreshold(
+                        g, 255,
+                        cv2.ADAPTIVE_THRESH_GAUSSIAN_C,
+                        cv2.THRESH_BINARY,
+                        blockSize=25,
+                        C=4
+                    )
+
+    h, w = thresh.shape[:2]
+
+    # I want @170.00 170円 here, if above 5.0 consider that's not the one
     if w/h > 5.0:
         print("you can ignore it w/h>4.0,it's too wide")
-        # continue
+        continue
     if w/h < 2.0: 
         continue
 
